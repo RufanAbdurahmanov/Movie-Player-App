6daf57e40b114e46b07234b9bfb5e1e82190218c
RufanAbdurahmanov <rufan.abdurahmanov@gmail.com>
2022-05-30 18:27:20 +0400

    Initial Commit

---
+++ Movie App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Movie App
+//
+//  Created by Rufan Abdurahmanov on 30.05.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
